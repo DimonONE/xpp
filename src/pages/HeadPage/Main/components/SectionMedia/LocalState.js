4d6e342e4cc2dda39846ebc162a0e2dcad16ca5d
Dimon Beyba <dmezey@mail.ua>
2021-05-10 19:35:10 +0300

    refactoring logic SectionMedia and addition effects

---
+++ src/pages/HeadPage/Main/components/SectionMedia/LocalState.js
@@ -0,0 +1,167 @@
+import media__item_img_1 from "../../../../../img/media__item-img-1.png";
+import media__item_img_2 from "../../../../../img/media__item-img-2.png";
+import media__item_img_3 from "../../../../../img/media__item-img-3.png";
+import media__item_img_4 from "../../../../../img/media__item-img-4.png";
+import media__item_img_5 from "../../../../../img/media__item-img-5.jpg";
+import media__item_img_6 from "../../../../../img/media__item-img-6.png";
+import media__item_img_7 from "../../../../../img/media__item-img-7.jpg";
+import media__item_img_8 from "../../../../../img/media__item-img-8.png";
+import media__item_img_9 from "../../../../../img/media__item-img-9.png";
+import media__item_img_10 from "../../../../../img/media__item-img-10.png";
+import media__item_img_11 from "../../../../../img/media__item-img-11.jpg";
+import media__item_img_12 from "../../../../../img/media__item-img-12.jpg";
+import media__item_img_13 from "../../../../../img/media__item-img-13.jpg";
+import media__item_img_14 from "../../../../../img/media__item-img-14.jpg";
+import media__item_img_15 from "../../../../../img/media__item-img-15.jpg";
+import media__item_img_16 from "../../../../../img/media__item-img-16.jpg";
+import media__item_img_17 from "../../../../../img/media__item-img-17.jpg";
+
+export const localState = [
+  {
+    id: 1,
+    delay: 100,
+    link:
+      "https://finance.yahoo.com/news/xx-platform-advanced-trading-platform-174500642.html?.tsrc=fin-srch",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_1 + ")" },
+  },
+
+  {
+    id: 2,
+    delay: 200,
+    link:
+      "https://www.benzinga.com/pressreleases/21/02/ac19568881/xx-platform-an-advanced-trading-platform-for-investors-and-stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_2 + ")" },
+  },
+
+  {
+    id: 3,
+    delay: 300,
+    link: "http://www.digitaljournal.com/pr/4968984",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_3 + ")" },
+  },
+
+  {
+    id: 4,
+    delay: 400,
+    link: "https://icoholder.com/blog/safe-investments-myth-or-reality/",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_4 + ")" },
+  },
+
+  {
+    id: 5,
+    delay: 500,
+    link: "https://www.lelezard.com/en/news-19642322.html",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_5 + ")" },
+  },
+
+  {
+    id: 6,
+    delay: 600,
+    link:
+      "https://www.accesswire.com/628745/XX-Platform-An-Advanced-Trading-Platform-for-Investors-and-Stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_6 + ")" },
+  },
+
+  {
+    id: 7,
+    delay: 100,
+    link:
+      "https://aboutyourmag.info/2021/02/09/xx-platform-an-advanced-trading-platform-for-investors-and-stakers-about-your-online-magazine/",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_7 + ")" },
+  },
+
+  {
+    id: 8,
+    delay: 200,
+    link:
+      "http://markets.post-gazette.com/postgazette/news/read/40966765/xx_platform_an_advanced_trading_platform_for_investors_and_stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_8 + ")" },
+  },
+
+  {
+    id: 9,
+    delay: 300,
+    link:
+      "https://apnews.com/press-release/accesswire/technology-business-financial-technology-deflation-computer-and-data-security-1a7cb1537d834cb8e179bb0bf0a9ab39",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_9 + ")" },
+  },
+
+  {
+    id: 10,
+    delay: 400,
+    link: "https://cryptoshib.com/xx-platform-advanced-trading/",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_10 + ")" },
+  },
+
+  {
+    id: 11,
+    delay: 500,
+    link:
+      "https://crymarket.org/2021/02/09/xx-platform-an-advanced-trading-platform-for-investors-and-stakers/",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_11 + ")" },
+  },
+
+  {
+    id: 12,
+    delay: 600,
+    link:
+      "http://markets.financialcontent.com/streetinsider/news/read/40966765",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_12 + ")" },
+  },
+
+  {
+    id: 13,
+    delay: 100,
+    link:
+      "http://markets.financialcontent.com/startribune/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_13 + ")" },
+  },
+
+  {
+    id: 14,
+    delay: 200,
+    link: "https://markets.financialcontent.com/ibtimes/news/read/40966765",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_14 + ")" },
+  },
+
+  {
+    id: 15,
+    delay: 300,
+    link:
+      "https://markets.financialcontent.com/bostonherald/news/read/40966765",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_15 + ")" },
+  },
+
+  {
+    id: 16,
+    delay: 400,
+    link:
+      "http://business.theantlersamerican.com/theantlersamerican/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_16 + ")" },
+  },
+
+  {
+    id: 17,
+    delay: 500,
+    link:
+      "http://business.dailytimesleader.com/dailytimesleader/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers",
+    text: "Read",
+    dataStyle: { backgroundImage: "url(" + media__item_img_17 + ")" },
+  },
+];
--- src/pages/HeadPage/Main/components/SectionMedia/SectionMedia.jsx
+++ src/pages/HeadPage/Main/components/SectionMedia/SectionMedia.jsx
@@ -1,269 +1,28 @@
 import React from "react";
-import style from "./SectionMedia.module.css"
+import { Fade } from "react-awesome-reveal";
+import { localState } from "./LocalState.js"
 
 export const SectionMedia = (props) => {
     return(
       <section className="section media">
         <div className="container media__container">
-          <h2 className="heading font-size-40 media__heading text-center wow animate__animated animate__fadeInDown">
-            <b className="font-weight-700"> Mass media </b>
-          </h2>
+          <Fade direction="down">
+            <h2 className="heading font-size-40 media__heading text-center wow animate__animated animate__fadeInDown">
+              <b className="font-weight-700"> Mass media </b>
+            </h2>
+          </Fade>
           <div className="row media__row">
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_1}`}
-                ></div>
-                <a
-                  href="https://finance.yahoo.com/news/xx-platform-advanced-trading-platform-174500642.html?.tsrc=fin-srch"
-                  className="media__item-link"
-                  >Read</a
-                >
+            {localState.map( (e) => 
+              <div key={e.id} className="media__item-wrap col-auto wow animate__animated animate__fadeInUp">
+                <Fade direction="up" delay={e.delay}>
+                  <div className="media__item">
+                    <div className={`media__item-img`} style={e.dataStyle}></div>
+                    <a href={e.link}
+                      className="media__item-link">{e.text}</a>
+                  </div>
+                </Fade>
               </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.2s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_2}`}
-                ></div>
-                <a
-                  href="https://www.benzinga.com/pressreleases/21/02/ac19568881/xx-platform-an-advanced-trading-platform-for-investors-and-stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_3}`}
-                ></div>
-                <a
-                  href="http://www.digitaljournal.com/pr/4968984"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.4s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_4}`}
-                ></div>
-                <a
-                  href="https://icoholder.com/blog/safe-investments-myth-or-reality/"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_5}`}
-                ></div>
-                <a
-                  href="https://www.lelezard.com/en/news-19642322.html"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.6s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_6}`}
-                ></div>
-                <a
-                  href="https://www.accesswire.com/628745/XX-Platform-An-Advanced-Trading-Platform-for-Investors-and-Stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_7}`}
-                ></div>
-                <a
-                  href="https://aboutyourmag.info/2021/02/09/xx-platform-an-advanced-trading-platform-for-investors-and-stakers-about-your-online-magazine/"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.2s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_8}`}
-                ></div>
-                <a
-                  href="http://markets.post-gazette.com/postgazette/news/read/40966765/xx_platform_an_advanced_trading_platform_for_investors_and_stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_9}`}
-                ></div>
-                <a
-                  href="https://apnews.com/press-release/accesswire/technology-business-financial-technology-deflation-computer-and-data-security-1a7cb1537d834cb8e179bb0bf0a9ab39"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.4s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_10}`}
-                ></div>
-                <a
-                  href="https://cryptoshib.com/xx-platform-advanced-trading/"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_11}`}
-                ></div>
-                <a
-                  href="https://crymarket.org/2021/02/09/xx-platform-an-advanced-trading-platform-for-investors-and-stakers/"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.6s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_12}`}
-                ></div>
-                <a
-                  href="http://markets.financialcontent.com/streetinsider/news/read/40966765"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.1s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_13}`}
-                ></div>
-                <a
-                  href="http://markets.financialcontent.com/startribune/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.2s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_14}`}
-                ></div>
-                <a
-                  href="https://markets.financialcontent.com/ibtimes/news/read/40966765"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_15}`}
-                ></div>
-                <a
-                  href="https://markets.financialcontent.com/bostonherald/news/read/40966765"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.4s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_16}`}
-                ></div>
-                <a
-                  href="http://business.theantlersamerican.com/theantlersamerican/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
-            <div
-              className="media__item-wrap col-auto wow animate__animated animate__fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <div className="media__item">
-                <div
-                  className={`media__item-img ${style.media__item_img_17}`}
-                ></div>
-                <a
-                  href="http://business.dailytimesleader.com/dailytimesleader/news/read/40966765/XX_Platform_An_Advanced_Trading_Platform_for_Investors_and_Stakers"
-                  className="media__item-link"
-                  >Read</a
-                >
-              </div>
-            </div>
+            )}
           </div>
         </div>
       </section>
