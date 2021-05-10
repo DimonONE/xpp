d555364cb7ebeb01aef73665db9660833d5cb8ac
Dimon Beyba <dmezey@mail.ua>
2021-05-10 21:10:59 +0300

    adition effects SectionFounder

--- src/pages/HeadPage/Main/components/SectionDelegates/SectionDelegates.jsx
+++ src/pages/HeadPage/Main/components/SectionDelegates/SectionDelegates.jsx
@@ -1,23 +1,23 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
-import delegates__cards_list_item_icon_1 from "../../../../../img/delegates__cards-list-item-icon-1.svg"
-import delegates__cards_list_item_icon_2 from "../../../../../img/delegates__cards-list-item-icon-2.svg"
-import delegates__cards_list_item_icon_3 from "../../../../../img/delegates__cards-list-item-icon-3.svg"
-import delegates__cards_list_item_icon_4 from "../../../../../img/delegates__cards-list-item-icon-4.svg"
-import delegates__cards_list_item_icon_5 from "../../../../../img/delegates__cards-list-item-icon-5.svg"
-import delegates__cards_list_item_icon_6 from "../../../../../img/delegates__cards-list-item-icon-6.svg"
+import delegates__cards_list_item_icon_1 from "../../../../../img/delegates__cards-list-item-icon-1.svg";
+import delegates__cards_list_item_icon_2 from "../../../../../img/delegates__cards-list-item-icon-2.svg";
+import delegates__cards_list_item_icon_3 from "../../../../../img/delegates__cards-list-item-icon-3.svg";
+import delegates__cards_list_item_icon_4 from "../../../../../img/delegates__cards-list-item-icon-4.svg";
+import delegates__cards_list_item_icon_5 from "../../../../../img/delegates__cards-list-item-icon-5.svg";
+import delegates__cards_list_item_icon_6 from "../../../../../img/delegates__cards-list-item-icon-6.svg";
 
-import delegates__img from "../../../../../img/delegates__img.png"
+import delegates__img from "../../../../../img/delegates__img.png";
 
 export const SectionDelegates = (props) => {
-    return(
-      <section className="section delegates">
-        <div className="container delegates__container">
-          <div className="row delegates__row">
-            <div className="col-lg-7 delegates__main">
-              <div
-                className="delegates__main-texts wow animate__animated animate__fadeInLeft"
-              >
+  return (
+    <section className="section delegates">
+      <div className="container delegates__container">
+        <div className="row delegates__row">
+          <div className="col-lg-7 delegates__main">
+            <div className="delegates__main-texts wow">
+              <Fade direction="left">
                 <h2 className="heading font-size-40 delegates__heading">
                   <b className="font-weight-700">Delegates</b>
                 </h2>
@@ -26,17 +26,15 @@ export const SectionDelegates = (props) => {
                   of bets —
                   <b className="font-weight-600 dark-blue">XX Delegate.</b>
                 </div>
-              </div>
-              <div className="delegates__cards" data-mobile-tabs>
-                <div
-                  className="delegates__cards-pills d-flex"
-                  id="delegatesPills">
-                </div>
-                <div
-                  className="delegates__cards-item wow animate__animated animate__fadeInLeft"
-                  data-wow-delay="0.4s"
-                  data-mobile-tab="2"
-                >
+              </Fade>
+            </div>
+            <div className="delegates__cards" data-mobile-tabs>
+              <div
+                className="delegates__cards-pills d-flex"
+                id="delegatesPills"
+              ></div>
+              <Fade direction="left" delay={400}>
+                <div className="delegates__cards-item wow" data-mobile-tab="2">
                   <div
                     className="delegates__cards-type"
                     data-mobile-pill="2"
@@ -119,7 +117,9 @@ export const SectionDelegates = (props) => {
                         <div className="delegates__cards-list-item-heading">
                           Participation in conferences, meetups
                         </div>
-                        <div className="delegates__cards-list-item-text">yes</div>
+                        <div className="delegates__cards-list-item-text">
+                          yes
+                        </div>
                       </li>
                       <li className="delegates__cards-list-item">
                         <div className="delegates__cards-list-item-icon-wrap">
@@ -132,40 +132,41 @@ export const SectionDelegates = (props) => {
                         <div className="delegates__cards-list-item-heading">
                           Participation in voting
                         </div>
-                        <div className="delegates__cards-list-item-text">yes</div>
+                        <div className="delegates__cards-list-item-text">
+                          yes
+                        </div>
                       </li>
                     </ul>
                   </div>
                 </div>
-              </div>
+              </Fade>
             </div>
-            <div className="col-lg-5 delegates__img-wrap">
-              <img
-                src={delegates__img}
-                alt="Img"
-                className="delegates__img wow animate__animated animate__fadeIn"
-              />
-              <div
-                className="delegates__img-circle wow animate__animated animate__fadeIn"
-              ></div>
-              <div
-                className="delegates__img-card wow animate__animated animate__fadeInRight"
-              >
+          </div>
+          <div className="col-lg-5 delegates__img-wrap">
+            <img
+              src={delegates__img}
+              alt="Img"
+              className="delegates__img wow "
+            />
+            <div className="delegates__img-circle wow"></div>
+            <Fade direction="right">
+              <div className="delegates__img-card wow">
                 In order to create a Delegate account, <br />
                 it is necessary to “freeze”
                 <b className="font-weight-700">10,000 XXP tokens</b> <br />
                 and register in the user’s personal account.
               </div>
-              <div
-                className="delegates__img-text wow animate__animated animate__fadeInRight"
-              >
+            </Fade>
+            <Fade direction="right" delay={200}>
+              <div className="delegates__img-text wow">
                 The Delegate, being the creator of his staking pool, has the
                 right to independently set the size of commissions on payments,
                 the percentage of which can vary from 1% to 10%.
               </div>
-            </div>
+            </Fade>
           </div>
         </div>
-      </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
--- src/pages/HeadPage/Main/components/SectionFounder/SectionFounder.jsx
+++ src/pages/HeadPage/Main/components/SectionFounder/SectionFounder.jsx
@@ -1,74 +1,81 @@
 import React from "react";
-import founder__img from "../../../../../img/founder__img.jpg"
-import founder__info_icon from "../../../../../img/founder__info-icon.svg"
+import { Fade } from "react-awesome-reveal";
+
+import founder__img from "../../../../../img/founder__img.jpg";
+import founder__info_icon from "../../../../../img/founder__info-icon.svg";
 
 export const SectionFounder = (props) => {
-    return(
-        <section className="section founder">
-            <div className="container founder__container">
-            <div className="pattern">
-                <div className="pattern__img pattern__img--3"></div>
-                <div className="pattern__img pattern__img--7"></div>
-                <div className="pattern__img pattern__img--9"></div>
-                <div className="pattern__img pattern__img--11"></div>
-                <div className="pattern__img pattern__img--13"></div>
-            </div>
-            <div id="founderMobileHeading"></div>
-            <div className="row founder__row">
-                <div className="col-lg-5 founder__img-wrap">
-                <img
-                    src={founder__img}
-                    alt="Img"
-                    className="img-responsive founder__img wow animate__animated animate__fadeInLeft"
-                />
-                <div
-                    className="founder__info-wrap wow animate__animated animate__fadeInUp"
-                >
-                    <div className="founder__info">
-                    <div className="founder__info-heading">Yuriy Draganov</div>
-                    <div className="founder__info-text">
-                        Founder of XX Platform <br />
-                        Professional trader, financial analyst. <br />
-                        8 years on the market.
-                    </div>
-                    </div>
-                    <div
-                    className="founder__info-icon"
-                    style={{backgroundImage: 'url(' + founder__info_icon + ')'}}
-                    ></div>
-                </div>
+  return (
+    <section className="section founder">
+      <div className="container founder__container">
+        <div className="pattern">
+          <div className="pattern__img pattern__img--3"></div>
+          <div className="pattern__img pattern__img--7"></div>
+          <div className="pattern__img pattern__img--9"></div>
+          <div className="pattern__img pattern__img--11"></div>
+          <div className="pattern__img pattern__img--13"></div>
+        </div>
+        <div id="founderMobileHeading"></div>
+        <div className="row founder__row">
+          <div className="col-lg-5 founder__img-wrap">
+            <Fade direction="left" triggerOnce="true">
+              <img
+                src={founder__img}
+                alt="Img"
+                className="img-responsive founder__img wow"
+              />
+            </Fade>
+            <Fade direction="up" triggerOnce="true" duration={600}>
+              <div className="founder__info-wrap wow ">
+                <div className="founder__info">
+                  <div className="founder__info-heading">Yuriy Draganov</div>
+                  <div className="founder__info-text">
+                    Founder of XX Platform <br />
+                    Professional trader, financial analyst. <br />8 years on the
+                    market.
+                  </div>
                 </div>
                 <div
-                className="col-lg-7 founder__texts wow animate__animated animate__fadeInRight"
-                >
+                  className="founder__info-icon"
+                  style={{ backgroundImage: "url(" + founder__info_icon + ")" }}
+                ></div>
+              </div>
+            </Fade>
+          </div>
+          <div className="col-lg-7 founder__texts wow">
+            <Fade direction="right" triggerOnce="true">
+              <div>
                 <h2
-                    className="heading font-size-40 founder__heading"
-                    data-replace="founderMobileHeading"
+                  className="heading font-size-40 founder__heading"
+                  data-replace="founderMobileHeading"
                 >
-                    The founder’s <b className="font-weight-700">statement</b>
+                  The founder’s <b className="font-weight-700">statement</b>
                 </h2>
                 <div className="founder__text">
-                    Trading and investments are trending today. Thousands of stocks
-                    of various companies are represented on hundreds of markets...
-                    it is difficult to process such a volume of information even for
-                    professionals, and it is almost impossible for beginning traders
-                    or investors. Moreover, such activities take a lot of time and
-                    effort, and the results often leave much to be desired...
+                  Trading and investments are trending today. Thousands of
+                  stocks of various companies are represented on hundreds of
+                  markets... it is difficult to process such a volume of
+                  information even for professionals, and it is almost
+                  impossible for beginning traders or investors. Moreover, such
+                  activities take a lot of time and effort, and the results
+                  often leave much to be desired...
                 </div>
                 <div className="founder__text">
-                    Therefore, we decided to create a platform that will allow you
-                    to succeed not wasting your time on reading economic and
-                    business reports. Surely you have neither the time nor the
-                    desire for this, and much “iron nerves”.
+                  Therefore, we decided to create a platform that will allow you
+                  to succeed not wasting your time on reading economic and
+                  business reports. Surely you have neither the time nor the
+                  desire for this, and much “iron nerves”.
                 </div>
                 <div className="founder__text">
-                    It is better to spend this time with your family, while the
-                    professionals from XX Platform will do everything on your
-                    behalf!
+                  It is better to spend this time with your family, while the
+                  professionals from XX Platform will do everything on your
+                  behalf!
                 </div>
-                </div>
-            </div>
-            </div>
-      </section>
-    )
-}
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
