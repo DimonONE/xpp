9f215c0bbecfabd2a4404695bd69cc2e62bec6e8
Dimon Beyba <dmezey@mail.ua>
2021-05-26 03:11:40 +0300

    adding section news

--- src/pages/HeadPage/Main/components/SectionNews/LocalState.js
+++ src/pages/HeadPage/Main/components/SectionNews/LocalState.js
@@ -6,36 +6,43 @@ import vector_2 from "../../../../../img/vector_2.png";
 import vector_3 from "../../../../../img/vector_3.png";
 import arrowUp from "../../../../../img/arrowUp.png";
 import arrowDown from "../../../../../img/arrowDown.png";
+import icon__all_news from "../../../../../img/icon__all_news.png";
+import ilonMask from "../../../../../img/IlonMask.png";
 
-export const localState = [
-  {
-    id: 1,
-    icon: hot_bit,
-    litl_ico: union,
-    sum: "0.7595",
-    arrival: "+0.00000000",
-    schedule: vector_1,
-    arrowUp: arrowUp,
-    arrowDown: arrowDown,
-  },
-  {
-    id: 2,
-    icon: hot_bit,
-    litl_ico: union,
-    sum: "1.0500",
-    arrival: "+1.0500",
-    schedule: vector_2,
-    arrowUp: arrowUp,
-    arrowDown: arrowDown,
-  },
-  {
-    id: 3,
-    icon: crex24,
-    litl_ico: union,
-    sum: "1.0154",
-    arrival: "-1.0023",
-    schedule: vector_3,
-    arrowUp: arrowUp,
-    arrowDown: arrowDown,
-  },
-];
+export const localState = {
+  navBlock: [
+    {
+      id: 1,
+      icon: hot_bit,
+      litl_ico: union,
+      sum: "0.7595",
+      arrival: "+0.00000000",
+      schedule: vector_1,
+      arrowUp: arrowUp,
+      arrowDown: arrowDown,
+    },
+    {
+      id: 2,
+      icon: hot_bit,
+      litl_ico: union,
+      sum: "1.0500",
+      arrival: "+1.0500",
+      schedule: vector_2,
+      arrowUp: arrowUp,
+      arrowDown: arrowDown,
+    },
+    {
+      id: 3,
+      icon: crex24,
+      litl_ico: union,
+      sum: "1.0154",
+      arrival: "-1.0023",
+      schedule: vector_3,
+      arrowUp: arrowUp,
+      arrowDown: arrowDown,
+    },
+  ],
+  news: [{ id: 1 }, { id: 2 }, { id: 3 }],
+  icon_all_news: icon__all_news,
+  ilonMask: ilonMask,
+};
--- src/pages/HeadPage/Main/components/SectionNews/SectionNews.jsx
+++ src/pages/HeadPage/Main/components/SectionNews/SectionNews.jsx
@@ -16,7 +16,7 @@ export const SectionNews = (props) => {
           <b className="font-weight-700">Торговые</b> пары
         </h2>
         <div className={`${style.news_nav}`}>
-          {localState.map((e) => (
+          {localState.navBlock.map((e) => (
             <span key={e.id} className={`${style.block}`}>
               <div className={`${style.block_head}`}>
                 <div className={`${style.news_nav__icon}`}>
@@ -112,7 +112,85 @@ export const SectionNews = (props) => {
                 </div>
               </span>
             </span>
-            <span className={style.section_right}></span>
+            <span className={style.section_right}>
+              {localState.news.map((e) => (
+                <div key={e.id} className={style.news_item}>
+                  <span className={style.news_item__img}>
+                    <img src={localState.ilonMask} alt="NoN" />
+                  </span>
+                  <span className={style.news_item__info}>
+                    <div>
+                      <h2 className={style.text_info}>
+                        Илон Маск опубликовал фото Starbase - и токен взлетел на
+                        6000%
+                      </h2>
+                      <div className={style.items__info}>
+                        <div className={style.block_link}>
+                          <a className={style.link} href="/">
+                            Подробнее
+                          </a>
+                          <svg
+                            width="16"
+                            height="12"
+                            viewBox="0 0 16 12"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M1 6H15M15 6L10.0222 1M15 6L10.0222 11"
+                              stroke="#FF8225"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </svg>
+                        </div>
+                        <div className={style.data}>
+                          <svg
+                            width="15"
+                            height="15"
+                            viewBox="0 0 15 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M13.0664 1.17188H11.7188V0.46875C11.7188 0.209883 11.5089 0 11.25 0C10.9911 0 10.7812 0.209883 10.7812 0.46875V1.17188H4.21875V0.46875C4.21875 0.209883 4.0089 0 3.75 0C3.4911 0 3.28125 0.209883 3.28125 0.46875V1.17188H1.93359C0.867393 1.17188 0 2.03927 0 3.10547V13.0664C0 14.1326 0.867393 15 1.93359 15H13.0664C14.1326 15 15 14.1326 15 13.0664V3.10547C15 2.03927 14.1326 1.17188 13.0664 1.17188ZM1.93359 2.10938H3.28125V2.57812C3.28125 2.83699 3.4911 3.04688 3.75 3.04688C4.0089 3.04688 4.21875 2.83699 4.21875 2.57812V2.10938H10.7812V2.57812C10.7812 2.83699 10.9911 3.04688 11.25 3.04688C11.5089 3.04688 11.7188 2.83699 11.7188 2.57812V2.10938H13.0664C13.6157 2.10938 14.0625 2.55621 14.0625 3.10547V4.21875H0.9375V3.10547C0.9375 2.55621 1.38434 2.10938 1.93359 2.10938ZM13.0664 14.0625H1.93359C1.38434 14.0625 0.9375 13.6157 0.9375 13.0664V5.15625H14.0625V13.0664C14.0625 13.6157 13.6157 14.0625 13.0664 14.0625Z"
+                              fill="#606060"
+                            />
+                          </svg>
+                          <p>18.05.2021</p>
+                        </div>
+                      </div>
+                    </div>
+                  </span>
+                </div>
+              ))}
+
+              <a href="/" className={`${style.all_news} ${style.block}`}>
+                <div className={style.all_news}>
+                  <div>
+                    <img src={localState.icon_all_news} alt="icon_all_news" />
+                  </div>
+                  <p>Все новости</p>
+                </div>
+                <div>
+                  <svg
+                    width="23"
+                    height="17"
+                    viewBox="0 0 23 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1 8.5H22M22 8.5L14.5333 1M22 8.5L14.5333 16"
+                      stroke="#C4C4C4"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+              </a>
+            </span>
           </div>
         </div>
       </div>
